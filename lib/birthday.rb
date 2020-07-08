0b0659d33982ed9d45b8c8fefbd5bcb3eca79f69
Jackson Reynolds <jwreynold@gmail.com>
2020-07-08 21:16:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,10 +5,10 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age| 
+  birthday_kids.each do |kids_name, age|
     if age <=12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else 
+    else
       puts "You are too olf for this."
     end #if
   end #do
